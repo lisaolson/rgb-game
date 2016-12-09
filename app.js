2756a2118cbb0c41b8751d7ae126f9b57dceb146
Lisa Olson <olson.lisa94@gmail.com>
2016-12-09 13:21:05 -0800

    begins logic for easy/hard button

--- app.js
+++ app.js
@@ -7,6 +7,33 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.querySelector('#message');
 var h1 = document.querySelector('h1');
+var resetButton = document.querySelector('#reset');
+var easyBtn = document.querySelector('#easy');
+var hardBtn = document.querySelector('#hard');
+
+easyBtn.addEventListener('click', function(){
+  hardBtn.classList.remove('selected');
+  easyBtn.classList.add('selected');
+});
+
+hardBtn.addEventListener('click', function(){
+  hardBtn.classList.add('selected');
+  easyBtn.classList.remove('selected');
+});
+
+resetButton.addEventListener('click', function(){
+  // generate all new colors
+  colors = generateRandomColors(6);
+  // pick new random color from array
+  pickedColor = pickColor();
+  // change colorDisplay to match picked color
+  colorDisplay.textContent = pickedColor;
+  // change colors of squares
+  for(var i = 0; i < squares.length; i++){
+    squares[i].style.background = colors[i];
+  }
+  h1.style.background = "#232323";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -21,6 +48,7 @@ for(var i = 0; i < squares.length; i++) {
     //compare color to pickedColor
     if(clickedColor === pickedColor){
       messageDisplay.textContent = "Correct!";
+      resetButton.textContent = "Play Again?";
       changeColors(clickedColor);
       h1.style.background = clickedColor;
     } else {
